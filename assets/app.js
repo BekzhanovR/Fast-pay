ed975e4d922962052593b92a1c7b96bd3ebc4838
BekzhanovR <bekzhanovofficial@gmail.com>
2024-09-11 20:51:35 +0500

    DEMO VERSION FINISHED

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+const priceSelect = document.getElementById('priceSelect');
+const otherPriceInput = document.getElementById('otherPriceInput');
+const payButton = document.querySelector('.pay')
+
+priceSelect.addEventListener('change', function() {
+    const selectedOption = priceSelect.options[priceSelect.selectedIndex];
+    
+    if (selectedOption.classList.contains('other')) {
+        otherPriceInput.classList.add('active');
+        otherPriceInput.disabled = false;
+    } else {
+        otherPriceInput.classList.remove('active');
+        otherPriceInput.disabled = true;
+    }
+});
+
+payButton.addEventListener('click', function() {
+    let amount;     
+    const selectedOption = priceSelect.options[priceSelect.selectedIndex];
+    
+    if (selectedOption.classList.contains('other')) {
+        amount = otherPriceInput.value;
+    } else {
+        amount = selectedOption.value;
+    }
+
+    const ussdCode = `*880*3*5614682205451612*${amount}`
+    window.location.href = "tel:" + ussdCode
+});
